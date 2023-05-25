3cbc82332e2728f0abe10dd79383d4a96c0d70fc
Vanessa Hoamea <vanessahoamea@yahoo.com>
2023-05-25 14:31:33 +0300

    split services into tabs

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,21 +1,59 @@
+import { useEffect } from "react";
 import News from "../components/News";
 import "../assets/css/Main.css";
 
 export default function Main()
 {
+    useEffect(() => {
+        document.getElementsByClassName("default")[0].click();
+    }, []);
+
+    function switchTab(event, tabId)
+    {
+        [...document.querySelectorAll(".tab-content")].map((element) => {
+            element.style.display = "none";
+        });
+        document.getElementById(tabId).style.display = "block";
+
+        [...document.querySelectorAll(".tab-button")].map((element) => {
+            element.className = element.className.split(" active")[0];
+        })
+        event.target.className += " active";
+    }
+
     return (
         <div className="main-content">
-            <h1>🌍 Welcome to Trip Planner! ✈️</h1>
-            <p>Our goal is to make trip planning easier for you.</p>
-            <p>Feel free to browse the services we offer.</p>
+            <div className="top-text">
+                <h1>🌍 Welcome to Trip Planner! ✈️</h1>
+                <p>Our goal is to make trip planning easier for you.</p>
+                <p>Feel free to browse the services we offer.</p>
+            </div>
 
-            {/* Maps + time zone */}
+            <div className="tab">
+                <button className="tab-button default" onClick={(e) => switchTab(e, "maps")}>Maps</button>
+                <button className="tab-button" onClick={(e) => switchTab(e, "translator")}>Translator</button>
+                <button className="tab-button" onClick={(e) => switchTab(e, "news")}>News</button>
+                <button className="tab-button" onClick={(e) => switchTab(e, "videos")}>Videos</button>
+            </div>
 
-            {/* Translator + text-to-speech */}
+            <div id="maps" className="tab-content">
+                {/* Maps + time zone */}
+                TBA 1
+            </div>
 
-            <News />
+            <div id="translator" className="tab-content">
+                {/* Translator + text-to-speech */}
+                TBA 2
+            </div>
+
+            <div id="news" className="tab-content">
+                <News />
+            </div>
             
-            {/* Video search */}
+            <div id="videos" className="tab-content">
+                {/* Video search */}
+                TBA 4
+            </div>
         </div>
     );
 }
